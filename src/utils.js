352dd557ee7fd48bca0e376c81cce1455ec69b44
Corey <coreyarice@gmail.com>
2018-11-18 16:28:17 -0600

    Add index to github pages with links to branches

--- src/utils.js
+++ src/utils.js
@@ -27,3 +27,24 @@ module.exports.getGHPagesUrl = function getGHPagesUrl(ghUrl) {
   
   return ghPagesUrl;
 };
+
+  module.exports.createHTML = function(baseURL, links){
+    return `
+    <!DOCTYPE html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>SVH Storybook</title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://unpkg.com/github-markdown-css@2.10.0/github-markdown.css">
+        </head>
+        <body>
+            <h1>Storybook Branch Links</h1>
+            <ul>
+              ${links.map( link => `<li><a href=${baseURL + link}><p>${link}</p></a></li>`).join('')}
+            </ul>
+        </body>
+    </html>
+    `
+};
